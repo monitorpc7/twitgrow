94b496aabfd79153597b2c5d0e486e229b68dc80
DevDhira <dhira2598@gmail.com>
2023-07-01 13:01:23 +0530

    second commit

--- app/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import get_reply
-
-urlpatterns = [
-    path('get-reply/', get_reply, name='get_reply'),
-]
--- app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-# Create your views here.
-
-@api_view(['POST','GET'])
-def get_reply(request):
-    return Response({'status':'Working'})
