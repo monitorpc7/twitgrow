e7bd4407ff539826456642bdda004210ae4d356a
DevDhira <dhira2598@gmail.com>
2023-07-01 14:59:02 +0530

    second commit

--- example/views.py
+++ example/views.py
@@ -3,14 +3,20 @@ from datetime import datetime
 
 from django.http import HttpResponse
 
+from rest_framework.response import Response
+
+# def index(request):
+#     now = datetime.now()
+#     html = f'''
+#     <html>
+#         <body>
+#             <h1>Hello from Vercel!</h1>
+#             <p>The current modified time is { now }.</p>
+#         </body>
+#     </html>
+#     '''
+#     return HttpResponse(html)
+
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current modified time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    now = datetime.now()  
+    return Response({"time":str(now)})
